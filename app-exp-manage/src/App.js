24bfd118bd26083301d39e08ca788665d24a809e
Julie Jourdrouin <jourdrouin.julie@gmail.com>
2021-02-18 09:57:54 +0100

    test mode dark

--- app-exp-manage/src/App.js
+++ app-exp-manage/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ModeDark from "../src/components/ModeDark";
+import "./scss/styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Gestionnaire de dépenses</h1>
+      <ModeDark />
+
     </div>
   );
 }
